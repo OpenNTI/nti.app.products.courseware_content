474c3b7c1e8623fe7809cd5bc9e13485cf68f28b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-21 02:43:09 +0000

    use actual ntiincludeannotationgraphics object
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_content/trunk@107053 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_content/docutils/translators.py
+++ src/nti/app/products/courseware_content/docutils/translators.py
@@ -15,21 +15,23 @@ from nti.contentlibrary_rendering.docutils.translators import TranslatorMixin
 
 from nti.contentlibrary_rendering.docutils.interfaces import IRSTToPlastexNodeTranslator
 
+from nti.contentrendering.plastexpackages.ntilatexmacros import ntiincludeannotationgraphics
 
 @interface.implementer(IRSTToPlastexNodeTranslator)
 class CourseAssetToPlastexNodeTranslator(TranslatorMixin):
 
-    __name__ = "course-asset"
+    __name__ = "course_asset"
 
     def translate(self, rst_node, tex_doc, tex_parent):
         result = tex_doc.createElement('figure')
         # process include graphics
         options = dict()  # graphics settings
-        grphx = tex_doc.createElement('ntiincludeannotationgraphics')
+        grphx = ntiincludeannotationgraphics()
         grphx.setAttribute('file', rst_node['uri'])
         grphx.setAttribute('options', options)
+        # TODO: Handle style and scale
         for name in ('height', 'width'):
-            value = rst_node.options[name]
+            value = rst_node.attributes.get(name, None)
             if value:
                 try:
                     float(value)  # unitless
