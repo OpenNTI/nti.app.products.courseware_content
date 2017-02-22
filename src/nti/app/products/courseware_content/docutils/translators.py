34443b6261d11856fc4e289782fd80d1dccdc821
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-22 18:50:27 +0000

    use utils method
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_content/trunk@107180 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_content/docutils/translators.py
+++ src/nti/app/products/courseware_content/docutils/translators.py
@@ -13,12 +13,12 @@ from zope import interface
 
 from nti.base._compat import unicode_
 
+from nti.app.contentlibrary_rendering.docutils.utils import process_rst_figure
+
 from nti.contentlibrary_rendering.docutils.translators import build_nodes
 from nti.contentlibrary_rendering.docutils.translators import TranslatorMixin
 
 from nti.contentlibrary_rendering.docutils.interfaces import IRSTToPlastexNodeTranslator
-
-from nti.contentrendering.plastexpackages.ntilatexmacros import ntiincludeannotationgraphics
 
 
 @interface.implementer(IRSTToPlastexNodeTranslator)
@@ -29,39 +29,7 @@ class CourseAssetToPlastexNodeTranslator(TranslatorMixin):
     def do_translate(self, rst_node, tex_doc, tex_parent):
         # start pushing the rst_nodes
         tex_doc.px_toggle_skip()
-        result = tex_doc.createElement('figure')
-
-        # attribute settings
-        options = dict()
-        grphx = ntiincludeannotationgraphics()
-        grphx.setAttribute('file', rst_node['uri'])
-        grphx.setAttribute('options', options)
-
-        # alternative text settings
-        value = rst_node.attributes.get('alt', None)
-        if value:  # alttext
-            grphx.setAttribute('alttext', value)
-            result.setAttribute('title', value)
-
-        # dimension settings
-        value = rst_node.attributes.get('scale', None)
-        if value:
-            options['scale'] = value if value <= 1 else value / 100.0
-        else:
-            for name in ('height', 'width'):
-                value = rst_node.attributes.get(name, None)
-                if value:
-                    try:
-                        float(value)  # unitless
-                        options[name] = '%spx' % (value)
-                    except (ValueError):
-                        options[name] = value
-
-        # add to set lineage
-        result.append(grphx)
-
-        # process image and return
-        grphx.process_image()
+        result, _ = process_rst_figure(rst_node, tex_doc)
         return result
 
     def do_legend(self, rst_node, figure, caption, tex_doc):
