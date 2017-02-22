a6653ce9d7d4f3f780b232a415d42b16d31b1a87
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-22 19:16:13 +0000

    use utils method
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_content/trunk@107186 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_content/docutils/directives.py
+++ src/nti/app/products/courseware_content/docutils/directives.py
@@ -9,8 +9,6 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import os
-
 from zope import interface
 
 from docutils import nodes
@@ -19,38 +17,11 @@ from docutils.parsers.rst import directives
 
 from docutils.parsers.rst.directives.images import Figure
 
-from nti.app.contentfile.view_mixins import is_oid_external_link
-from nti.app.contentfile.view_mixins import get_file_from_oid_external_link
-
-from nti.app.contentfolder.utils import is_cf_io_href
-from nti.app.contentfolder.utils import get_file_from_cf_io_url
+from nti.app.contentlibrary_rendering.docutils.utils import is_datasever_asset
+from nti.app.contentlibrary_rendering.docutils.utils import get_datasever_asset
+from nti.app.contentlibrary_rendering.docutils.utils import save_to_course_assets
 
 from nti.app.products.courseware_content.docutils.nodes import course_asset
-
-from nti.cabinet.filer import transfer_to_native_file
-
-COURSE_ASSETS = 'Images/CourseAssets'
-
-
-def is_course_asset(uri):
-    return is_cf_io_href(uri) or is_oid_external_link(uri)
-
-
-def get_course_asset(uri):
-    if is_cf_io_href(uri):
-        return get_file_from_cf_io_url(uri)
-    return get_file_from_oid_external_link(uri)
-
-
-def save_to_disk(asset, out_dir=None):
-    out_dir = out_dir or os.getcwd()
-    out_dir = os.path.join(out_dir, COURSE_ASSETS)
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    path = os.path.join(out_dir, asset.filename)
-    transfer_to_native_file(asset, path)
-    result = os.path.join(COURSE_ASSETS, asset.filename)
-    return result
 
 
 class CourseAsset(Figure):
@@ -61,7 +32,7 @@ class CourseAsset(Figure):
 
     def run(self):
         reference = directives.uri(self.arguments[0]) or u''
-        if not reference or not is_course_asset(reference):
+        if not reference or not is_datasever_asset(reference):
             raise self.error(
                 'Error in "%s" directive: "%s" is not a valid href value for '
                 'a course asset.' % (self.name, reference))
@@ -72,7 +43,7 @@ class CourseAsset(Figure):
                 'Error in "%s" directive: image is not a valid value for '
                 'the "figwidth" option.' % self.name)
 
-        asset = get_course_asset(reference)
+        asset = get_datasever_asset(reference)
         if asset is None:
             raise self.error(
                 'Error in "%s" directive: course asset "%" is missing'
@@ -91,13 +62,13 @@ class CourseAsset(Figure):
         node_attributes.update(figure_node.attributes)
 
         # creates the course asset, don't include the image node
-        course_asset_node = course_asset('', 
+        course_asset_node = course_asset('',
                                          *fig_children[1:],
                                          **node_attributes)
 
-        # when this directive runs the we assume the directory 
+        # when this directive runs the we assume the directory
         # to save the resource has been set
-        course_asset_node['uri'] = save_to_disk(asset)
+        course_asset_node['uri'] = save_to_course_assets(asset)
         return [course_asset_node]
 
 
