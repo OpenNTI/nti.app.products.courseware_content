1eb0ed90c81fecd9e37e8845ff55b9ed2564ddd2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-23 03:25:36 +0000

    require nti.contenttypes.courses; add / update supporting files
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_content/trunk@120294 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- README.rst
+++ README.rst
@@ -1,3 +1,9 @@
-==========
+===================================
 nti.app.products.courseware_content
-==========
+===================================
+
+.. image:: https://travis-ci.org/NextThought/nti.app.products.courseware_content.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.app.products.courseware_content
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.app.products.courseware_content/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.app.products.courseware_content?branch=master
--- setup.py
+++ setup.py
@@ -1,39 +1,60 @@
 import codecs
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0'
 
 entry_points = {
-	'console_scripts': [
-	],
-	"z3c.autoinclude.plugin": [
-		'target = nti.app.products',
-	],
+    'console_scripts': [
+    ],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app.products',
+    ],
 }
 
+
+TESTS_REQUIRE = [
+    'nti.app.testing',
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
-	name='nti.app.products.courseware_content',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="Umbrella product to support integrated courseware and content",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='pyramid course content',
-	classifiers=[
-		'Framework :: Pyramid',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: Implementation :: CPython'
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti', 'nti.app', 'nti.app.products'],
-	install_requires=[
-		'setuptools'
-	],
-	entry_points=entry_points
+    name='nti.app.products.courseware_content',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="Umbrella product to support integrated courseware and content",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords='pyramid course content',
+    classifiers=[
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    url="https://github.com/NextThought/nti.app.products.courseware_content",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app', 'nti.app.products'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+		'setuptools',
+        'nti.contentlibrary',
+        'nti.contenttypes.courses',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points,
 )
