1ebe93cdb435cedd1ebf91d7f3d739cb1f74ca4d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-25 02:07:10 +0000

    don't change content if not modified
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_content/trunk@120469 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_content/docutils/operators.py
+++ src/nti/app/products/courseware_content/docutils/operators.py
@@ -20,6 +20,8 @@ from zope.cachedescriptors.property import Lazy
 
 from nti.app.products.courseware.resources.utils import is_internal_file_link
 
+from nti.app.products.courseware.utils import transfer_resource_from_filer
+
 from nti.app.products.courseware.utils.exporter import save_resource_to_filer
 
 from nti.base._compat import text_
@@ -27,11 +29,12 @@ from nti.base._compat import bytes_
 
 from nti.contentlibrary.interfaces import IContentOperator
 from nti.contentlibrary.interfaces import IRenderableContentPackage
+from nti.contentlibrary.interfaces import IContentPackageImporterUpdater
+
+from nti.contenttypes.courses.interfaces import NTI_COURSE_FILE_SCHEME
 
 
-@interface.implementer(IContentOperator)
-@component.adapter(IRenderableContentPackage)
-class RenderablePackageContentOperator(object):
+class OperatorMixin(object):
 
     def __init__(self, *args):
         pass
@@ -42,30 +45,31 @@ class RenderablePackageContentOperator(object):
         pattern = re.compile(pattern, re.VERBOSE | re.UNICODE)
         return pattern
 
-    def _process_course_figure(self, input_lines, filer, idx, result):
-        line = input_lines[idx]
+
+@interface.implementer(IContentOperator)
+@component.adapter(IRenderableContentPackage)
+class RenderablePackageContentOperator(OperatorMixin):
+
+    def _process(self, line, filer, result):
+        modified = False
         m = self._figure_pattern.match(line)
-        if m is None:
-            return False
-        reference = m.groups()[0]
-        if is_internal_file_link(reference):    
-            internal = save_resource_to_filer(reference, filer, False)
-            line = re.sub(reference, internal, line)
-            result.append(line)
-            return True
-        return False
-    
-    def replace_all(self, content, filer):
-        idx = 0
-        result = []
+        if m is not None:
+            reference = m.groups()[0]
+            if is_internal_file_link(reference):
+                internal = save_resource_to_filer(reference, filer, False)
+                line = re.sub(reference, internal, line)
+                modified = True
+        result.append(line)
+        return modified
+
+    def _replace_all(self, content, filer, result):
+        modified = False
         input_lines = statemachine.string2lines(content)
         input_lines = statemachine.StringList(input_lines, '<string>')
-        while idx < len(input_lines):
-            matched = self._process_course_figure(input_lines, filer, idx, result)
-            if not matched:
-                result.append(input_lines[idx])
-            idx += 1
-        return u'\n'.join(result)
+        for idx in range(len(input_lines)):
+            modified = self._process(input_lines[idx],
+                                     filer, result) or modified
+        return modified
 
     def operate(self, content, unused_context=None, **kwargs):
         if not content:
@@ -76,7 +80,47 @@ class RenderablePackageContentOperator(object):
         is_bytes = isinstance(content, bytes)
         content = text_(content) if is_bytes else content
         try:
-            content = self.replace_all(content, filer)
+            result = []
+            modified = self._replace_all(content, filer, result)
+            if modified:
+                content = u'\n'.join(result)
         except Exception:
             logger.exception("Cannot operate on content")
         return bytes_(content) if is_bytes else content
+
+
+@component.adapter(IRenderableContentPackage)
+@interface.implementer(IContentPackageImporterUpdater)
+class _RenderableContentPackageImporterUpdater(OperatorMixin):
+
+    def _process(self, package, line, result, source_filer, target_filer):
+        modified = False
+        m = self._figure_pattern.match(line)
+        if m is not None:
+            reference = m.groups()[0]
+            if reference.startswith(NTI_COURSE_FILE_SCHEME):
+                href, unused = transfer_resource_from_filer(reference, package,
+                                                            source_filer, target_filer)
+                if href:
+                    line = re.sub(reference, href, line)
+        result.append(line)
+        return modified
+
+    def process_content(self, package, source_filer, target_filer):
+        result = []
+        modified = False
+        content = text_(package.contents or b'')
+        lines = statemachine.string2lines(content)
+        lines = statemachine.StringList(lines, '<string>')
+        for idx in range(len(lines)):
+            modified = self._process(package, lines[idx], result,
+                                     source_filer, target_filer) or modified
+        if modified:
+            content = u'\n'.join(result)
+            package.contents = bytes_(content)
+
+    def updateFromExternalObject(self, package, unused_external, *unused_args, **kwargs):
+        source_filer = kwargs.get('source_filer')
+        target_filer = kwargs.get('target_filer')
+        if source_filer is not None and target_filer is not None:
+            self.process_content(package, source_filer, target_filer)
